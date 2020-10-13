41d1edfebb880a02719922cb77ff0ce8c39f13ee
Christina Schweipert <c.schweipert@gmail.com>
2020-10-13 12:01:53 -0600

    GET request to recreation.gov

--- app.js
+++ app.js
@@ -13,7 +13,29 @@ app.use(bodyParser.urlencoded({
 
 app.use(express.static("public"));
 
+app.get("/", function (req, res) {
+    console.log("received first get request");
 
+    const url = 'https://www.recreation.gov/api/permits/250014/availability/month?start_date=2021-02-01T00:00:00.000Z&commercial_acct=false&is_lottery=false'
+    request(url, function (error, response, body) {
+        const data = JSON.parse(body);
+
+        const date = data.payload.availability[371].date_availability;
+        console.log(data)
+        console.log(date)
+
+        const iterate = (obj) => {
+            Object.keys(obj).forEach(key => {
+                console.log(`${key}: ${obj[key]}`)
+
+                if (typeof obj[key] === 'object') {
+                    iterate(obj[key])
+                }
+            })
+        }
+        iterate(date)
+    });
+});
 
 app.listen(3000, function () {
     console.log("Server started on port 3000");
